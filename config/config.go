95399408af4bfd709461de858d4de54f563136e9
Jesus Rodriguez <jesus.rodriguez@MacBook-Pro-de-admindesk.local>
2019-06-12 14:48:17 +0200

    viper config

--- config/config.go
+++ config/config.go
@@ -1,10 +1,30 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 type Config struct {
 	Name        string
 	Environment string
 }
 
 func Newconfig() *Config {
-	viper.echoSetConfigName("config")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+
+	instance := new(Config)
+	err := viper.Unmarshal(&instance)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return instance
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jesusrodr/gohelloworld
 
 go 1.12
 
-require github.com/spf13/viper v1.4.0 // indirect
+require (
+	github.com/sirupsen/logrus v1.2.0
+	github.com/spf13/viper v1.4.0
+)
